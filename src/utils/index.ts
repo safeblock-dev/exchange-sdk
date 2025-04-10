782b7bb883d271d57994a804101583fa01a0a737
knownout <a.slavinskii@icloud.com>
2025-04-10 03:48:27 +0300

    Add EventIdentifier into utils export

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,8 @@
+import { EventIdentifier } from "~/utils/event-bus"
 import { networkToSafeblockMap, safeblockToNetworkMap } from "~/utils/safeblock-mappings"
 
 export {
   networkToSafeblockMap,
-  safeblockToNetworkMap
+  safeblockToNetworkMap,
+  EventIdentifier
 }
