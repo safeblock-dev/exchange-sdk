c4c23babed43e8e65b0bb3eec35909390dfd4436
knownout <a.slavinskii@icloud.com>
2025-06-29 23:33:29 +0300

    Add balances fetch util into default export

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,10 @@
 import { EventIdentifier } from "~/utils/event-bus"
+import { fetchAccountBalances } from "~/utils/fetch-accounts-balances"
 import { networkToSafeblockMap, safeblockToNetworkMap } from "~/utils/safeblock-mappings"
 
 export {
   networkToSafeblockMap,
   safeblockToNetworkMap,
-  EventIdentifier
+  EventIdentifier,
+  fetchAccountBalances
 }
