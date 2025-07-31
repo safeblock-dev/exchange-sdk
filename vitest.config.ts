2409da03b59e423a5b0f8fca3f9f634641e93006
knownout <a.slavinskii@icloud.com>
2025-08-01 01:08:36 +0300

    Update test suits and fix some internal issues for smart routing fallback state

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,9 @@ import * as path from "node:path"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
+  test: {
+    testTimeout: 30_000
+  },
   resolve: {
     alias: [
       { find: "~", replacement: path.resolve(__dirname, "./src/") }
