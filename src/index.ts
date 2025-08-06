213248a0e8307915d3f544325bab640bdfd9dbd0
knownout <a.slavinskii@icloud.com>
2025-08-06 22:03:50 +0300

    Add contract addresses to the default export

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
+import { contractAddresses } from "~/config"
 import SafeBlock, { SdkConfig } from "~/sdk"
 import { ExtensionInitializeEnvironment } from "~/sdk/sdk-core"
+import SdkException, { SdkExceptionCode } from "~/sdk/sdk-exception"
 import SdkExtension, { PartialEventBus } from "~/sdk/sdk-extension"
 import { SdkMixins } from "~/sdk/sdk-mixins"
-import { ExecutorCallData, ExchangeRequest, ExchangeQuota, SimulatedRoute, BasicToken } from "~/types"
-import SdkException, { SdkExceptionCode } from "~/sdk/sdk-exception"
+import { BasicToken, ExchangeQuota, ExchangeRequest, ExecutorCallData, SimulatedRoute } from "~/types"
 
 export {
   SafeBlock,
   SdkException,
   SdkExceptionCode,
   SdkExtension,
+  contractAddresses,
 
   type SdkConfig,
   type ExecutorCallData,
